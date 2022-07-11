b1ef9b5265e8c9a44ab436ea1a4df3cf244a5dd8
MataGitUniat <mikes0174@hotmail.com>
2022-07-11 06:06:17 -0500

    Banner & Carousel

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import './Inicio.css';
+import NewLight from '../../shared/images/NewLight.jpg';
+
+
 
 function Inicio(props) {
+    const BannerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '25%',
+        height: '800px',
+        marginTop: '100px',
+        marginBottom: '100px',
+        border: '5px','solid':'black',
+        paddingLeft: '50px',
+
+
+    };
+    const BoxHowToPlay = {
+        display: 'flex',
+        flexDirection: 'column',
+    }
+
     return (
         <div>
-            <h1>Inicio</h1>
+            <img src={NewLight} style={BannerStyle} alt='' />
+            <p style={BoxHowToPlay}>
+                Caja 1
+            </p>
+            <p style={BoxHowToPlay}>
+                Caja 2
+            </p>
+            <p style={BoxHowToPlay}>
+                Caja 3
+            </p>
         </div>
     );
 }
