775ece9a206c54685192dae15b1f1c7a48598e0a
MataGitUniat <mikes0174@hotmail.com>
2022-07-17 13:22:01 -0500

    Apartado Como Jugar

--- src/components/ComoJugar/ComoJugar.js
+++ src/components/ComoJugar/ComoJugar.js
@@ -1,40 +1,221 @@
 import React from 'react';
-import Carousel from 'better-react-carousel';
+/* imagenes de  las clases del juego Hechicero , Cazador y Titan */
+import Clases1 from '../../shared/images/clases1.jpg';
+import Clases2 from '../../shared/images/clases2.jpg';
+import Clases3 from '../../shared/images/clases3.jpg';
+import Warlock from '../../shared/images/WarlockBanner.jpg';
+import SolarWarlock from '../../shared/images/SolarWarlockDawnBlade.PNG';
+import VoidWarlock from '../../shared/images/VoidWarlock.jpg';
+import ArcWarlock from '../../shared/images/StormcallerWarlock.jpg';
+import StasisWarlock from '../../shared/images/StasisWarlock.jpg';
+import Titan from '../../shared/images/TitanBanner.jpg';
+import SolarTitan from '../../shared/images/SunbreakerTitan.jpg';
+import VoidTitan from '../../shared/images/SentinelTitan.png';
+import ArcTitan from '../../shared/images/ArcTitan.jpg';
+import StasisTitan from '../../shared/images/StasisTitan.jpg';
+import Cazador from '../../shared/images/CazadorBanner.jpg';
+import SolarCazador from '../../shared/images/SolarCazador.jpg';
+import VoidCazador from '../../shared/images/VoidCazador.jpg';
+import ArcCazador from '../../shared/images/ArcCazador.jpg';
+import StasisCazador from '../../shared/images/StasisCazador.jpg';
+import Vanguardia from '../../shared/images/Vanguardia.jpeg';
+
 
 function ComoJugar(props) {
     return (
-        <div>
-            <h1>Como Jugar</h1>
-            <Carousel cols={2} rows={1} gap={10} loop>
-                <Carousel.Item>
-                    <img width="100%" src="https://picsum.photos/800/600?random=1" alt="" />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img width="100%" src="https://picsum.photos/800/600?random=2" alt="" />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img width="100%" src="https://picsum.photos/800/600?random=3"  alt=""/>
-                </Carousel.Item>
-                <Carousel.Item>
-                {/* anything you want to show in the grid */}
-            </Carousel.Item>
-            {/* ... */}
-            </Carousel>
-            <Carousel cols={2} rows={1} gap={10} loop>
-                <Carousel.Item>
-                    <img width="100%" src="https://picsum.photos/800/600?random=1" alt="" />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img width="100%" src="https://picsum.photos/800/600?random=2" alt=""/>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img width="100%" src="https://picsum.photos/800/600?random=3"alt="" />
-                </Carousel.Item>
-                <Carousel.Item>
-                {/* anything you want to show in the grid */}
-            </Carousel.Item>
-            {/* ... */}
-            </Carousel>
+        
+        <div class="Origin">
+            <br/>
+            <br/>
+            {/* Aqui va el Carousel que muestra todas las clases del juego Hechicero , Cazador y Titan */}
+            <div class="container-fluid">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={Clases1} class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={Clases2} class="d-block w-100" alt="..."/>
+                        </div>
+                        <div class="carousel-item">
+                        <img src={Clases3} class="d-block w-100" alt="..."/>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            
+            {/* Aqui va el Container de las Clases el cual explica todas las clases y subclases del juego */}
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row g-0 bg-light position-relative">
+                            <div class="col-md-6 mb-md-0 p-md-4">
+                                <img src={Warlock} class="w-100" alt="..."/>
+                            </div>
+                            <div class="col-md-6 p-4 ps-md-0">
+                            <h1 class="mt-0">Hechicero</h1>
+                            <h2>El Hechicero (Warlock en inglés) es una clase de Guardián que se especializa en la unión de "magia" de El Viajero y armas.
+                                 Como eruditos y guerreros de la Luz, los hechiceros se dedican a entender al Viajero y su poder.</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-bs-interval="10000">
+                                    <img src={VoidWarlock} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={SolarWarlock} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={ArcWarlock} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={StasisWarlock} class="d-block w-100" alt="..."/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row g-0 bg-light position-relative">
+                            <div class="col-md-6 mb-md-0 p-md-4">
+                                <img src={Titan} class="w-100" alt="..."/>
+                            </div>
+                            <div class="col-md-6 p-4 ps-md-0">
+                            <h1 class="mt-0">Titan</h1>
+                            <h2>El Titán (Titan en inglés) es una clase de Guardián que se especializa en el combate cercano, defensa y armamento pesado. 
+                                heroicos defensores de la Luz Se enfrentan a cualquier reto, con seguridad y decisión, 
+                                actuando como instrumentos de fuerza bruta de la voluntad del Viajero...</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-bs-interval="10000">
+                                    <img src={VoidTitan} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={SolarTitan} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={ArcTitan} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={StasisTitan} class="d-block w-100" alt="..."/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row g-0 bg-light position-relative">
+                            <div class="col-md-6 mb-md-0 p-md-4">
+                                <img src={Cazador} class="w-100" alt="..."/>
+                            </div>
+                            <div class="col-md-6 p-4 ps-md-0">
+                            <h1 class="mt-0">Cazador</h1>
+                            <h2>El Cazador (Hunter en inglés) es una clase de Guardián que se especializa en el combate táctico, la velocidad y el sigilo. 
+                                Son exploradores osados y asesinos expertos con los cuchillos y las armas de precisión. 
+                                Los cazadores descubren sus propios caminos y escriben sus propias leyes.</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-bs-interval="10000">
+                                    <img src={VoidCazador} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={SolarCazador} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={ArcCazador} class="d-block w-100" alt="..."/>
+                                </div>
+                                <div class="carousel-item" data-bs-interval="10000">
+                                    <img src={StasisCazador} class="d-block w-100" alt="..."/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                    <div class="card">
+                        <img src={Vanguardia} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                            <h1 class="card-title">Bienvenido a la Vanguardia</h1>
+                            <br/>
+                            <br/>
+                            <h2 class="card-text">Disfruta de elegir cualquiera de las clases y subclases que incluye el juego y por si te arrepientes de tu descision no importa,
+                            ya que bungie brinda 3 espacios para que puedas jugar con las clases y experimentes con ellas.
+                            </h2>
+                            <br/>
+                            <br/>
+                            <h3 class="card-text">Dirigete a la Torre para experimentar tus primeras misiones Guardian</h3>
+                            <br/>
+                            <br/>
+                            <a href="/Inicio" class="btn btn-dark">Volver al Inicio</a>
+                            </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -1,39 +1,80 @@
 import React from 'react';
 import './Inicio.css';
-import NewLight from '../../shared/images/NewLight.jpg';
-
-
+import NewLight from '../../shared/images/BannerNewLight.jpg';
+import ComoJugarExp from '../../shared/images/NewLightexpComoJugar.jpg';
+import TemporadasBanner from '../../shared/images/Temporadas Banner.jpg';
+import StatsBanner from '../../shared/images/StatsBanner.jpg';
 
 function Inicio(props) {
-    const BannerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        width: '25%',
-        height: '800px',
-        marginTop: '100px',
-        marginBottom: '100px',
-        border: '5px','solid':'black',
-        paddingLeft: '50px',
-
-
-    };
-    const BoxHowToPlay = {
-        display: 'flex',
-        flexDirection: 'column',
-    }
 
     return (
-        <div>
-            <img src={NewLight} style={BannerStyle} alt='' />
-            <p style={BoxHowToPlay}>
-                Caja 1
-            </p>
-            <p style={BoxHowToPlay}>
-                Caja 2
-            </p>
-            <p style={BoxHowToPlay}>
-                Caja 3
-            </p>
+        <div class="Origin">
+            <br/>
+            <br/>
+            <div class="container">
+            <div class="column">
+                <div class="row-3">
+                <img src={NewLight} class="img-thumbnail" alt="" />  
+                </div>
+                <br/>
+                <br/>
+                <div class="row-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Como Jugar
+                        </div>
+                        <div class="card-body">
+                            <img src={ComoJugarExp} class="card-img-top" alt="..."/>
+                            <h3 class="card-title">Bienvenido Guardian</h3>
+                            <h5 class="card-title"> ACCIÓN MMO DEFINITIVA </h5>
+                            <p class="card-text">
+                                Únete a millones de jugadores, crea tu guardián y colecciona armas y armaduras únicas para personalizar tu apariencia y tu estilo de juego. 
+                                Participa en desafiantes misiones cooperativas y en una variedad de modos PvP competitivos.
+                                 Vive la historia en evolución de Destiny 2 con amigos, o explora las estrellas con una escuadra de una sola persona.</p>
+                            <a href="/ComoJugar" class="btn btn-dark">Ir a la Pestaña Como Jugar</a>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <br/>
+                <div class="row-3">
+                    <div class="card">
+                        <div class="card-header">
+                            Temporadas
+                        </div>
+                        <div class="card-body">
+                            <img src={TemporadasBanner} class="card-img-top" alt="..."/>
+                            <h3 class="card-title">Conoce el Universo e historia de Destiny 2</h3>
+                            <h5 class="card-title"> El leviatan Regresa</h5>
+                            <p class="card-text">El Leviatán regresa
+                                Poder, depravación, venganza. 
+                                Lo que Calus desea ha tomado distintas formas con el pasar de los años. 
+                                Su nave, otrora opulenta, ahora abandonada y corrupta, regresó a nuestra galaxia con la mira puesta en la Pirámide durmiente de la Luna.</p>
+                            <a href="/Temporadas" class="btn btn-dark">Ir a la Pestaña Temporadas</a>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <br/>
+                <div class="row-3">
+                <div class="card">
+                        <div class="card-header">
+                            Sistema de Stats
+                        </div>
+                        <div class="card-body">
+                            <img src={StatsBanner} class="card-img-top" alt="..."/>
+                            <h3 class="card-title">Explora el apartado de Estadisticas</h3>
+                            <h5 class="card-title">se el mas Veloz, el mas resistente o el mas Super!!!</h5>
+                            <p class="card-text">Un apartado donde conoceras como funcionan todos los sistemas de stats dentro de destiny , desde las armas hasta las armaduras
+                             de esta manera obtendras un conocimiento muy bueno de las bases del videojuego </p>
+                            <a href="/SistemaDeStats" class="btn btn-dark">Ir a la Pestaña de Sistema de Stats</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br/>
+        <br/>
         </div>
     );
 }
