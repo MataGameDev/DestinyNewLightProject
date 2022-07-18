75295718329ed471ebe0708feed6a5b58252af76
MataGitUniat <mikes0174@hotmail.com>
2022-07-18 11:09:25 -0500

    Apartado de Temporadas

--- src/components/Temporadas/Temporadas.js
+++ src/components/Temporadas/Temporadas.js
@@ -1,9 +1,233 @@
 import React from 'react';
+import RedWar from '../../shared/images/GuerraRojaSeason1.jpg';
+import CoO from '../../shared/images/CurseOfOsiris.jpg';
+import Warmind from '../../shared/images/Warmind.jpg';
+import BlackArmory from '../../shared/images/BlackArmory.jpg';
+import JokersWild from '../../shared/images/JokersWild.jpg';
+import Opulence from '../../shared/images/opulence.jpg';
+import Impc from '../../shared/images/Impc.jpg';
+import Alba from '../../shared/images/Alba.jpg';
+import Dignos from '../../shared/images/Dignos.jpg';
+import Visitantes from '../../shared/images/Visitantes.jpg';
+import Caza from '../../shared/images/Caza.jpg';
+import Elegidos from '../../shared/images/Elegidos.jpg';
+import Simbionte from '../../shared/images/Simbionte.jpeg';
+import Perdidos from '../../shared/images/Perdidos.jpg';
+
+
+
+
+
+
+//<Link to={`/season${season}`}></Link>
 
 function Temporadas(props) {
     return (
-        <div>
-            <h1>Temporadas</h1>
+        <div class="Origin">
+            <br/>
+            <br/>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src={RedWar} class="w-100" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-info">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={CoO} class="w-100" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Warmind} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={BlackArmory} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src={JokersWild} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Opulence} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Impc} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Alba} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src={Dignos} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Visitantes} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Caza} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Elegidos} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere
+                                
+                                </a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src={Simbionte} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={Perdidos} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={"https://images.contentstack.io/v3/assets/blte410e3b15535c144/blt58ebe92f5c2985f3/620be743510c6b1c87b8a9d7/story-bg.jpg"} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src={"https://images.contentstack.io/v3/assets/blte410e3b15535c144/bltce2d3612f61af1b0/628baec01cd65960bcff1612/s17-media-screenshot1.jpg"} class="card-img-top" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card 
+                                title and make up the bulk of the card's content.</p>
+                                <a href="/Inicio" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <br/>
         </div>
     );
 }
